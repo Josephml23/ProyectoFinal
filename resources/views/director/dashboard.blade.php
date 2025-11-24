c4b22754ead87186979ca2ddac60d23c6b837789
Joseph Fredy Marcani Leyva <josephmarcani23@gmail.com>
2025-11-24 15:11:10 -0500

    Creacin de las pantallas que veran los usuarios

---
+++ resources/views/director/dashboard.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Panel del Director
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="text-lg font-bold text-blue-600">Bienvenido Director</h3>
+                    <p>Esta interfaz es exclusiva para ti. Aquí podrás gestionar toda la escuela.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
