ec955c2c59f268fcdfa2894c66e6e93a8fc39a4a
Joseph Fredy Marcani Leyva <josephmarcani23@gmail.com>
2025-11-24 15:06:48 -0500

    Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,35 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DirectorController;
+use App\Http\Controllers\ProfesorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// RUTA DASHBOARD INTELIGENTE
+// Cuando entran a /dashboard, Laravel decide a dónde mandarlos según su rol
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $role = auth()->user()->role;
+    
+    if ($role === 'director') {
+        return redirect()->route('director.dashboard');
+    } 
+    
+    return redirect()->route('profesor.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// --- ZONA DEL DIRECTOR ---
+Route::middleware(['auth', 'role:director'])->group(function () {
+    Route::get('/director/panel', [DirectorController::class, 'index'])->name('director.dashboard');
+});
+
+// --- ZONA DEL PROFESOR ---
+Route::middleware(['auth', 'role:profesor'])->group(function () {
+    Route::get('/profesor/panel', [ProfesorController::class, 'index'])->name('profesor.dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
