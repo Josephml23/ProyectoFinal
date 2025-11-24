059b37b99b671a5247d7906aa971cdf2b2d98253
Joseph Fredy Marcani Leyva <josephmarcani23@gmail.com>
2025-11-24 15:19:06 -0500

    Generación de Usuarios de Prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,27 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. Crear al Director (Usuario Único)
+        User::create([
+            'name' => 'Director General',
+            'email' => 'director@escuela.com',
+            'password' => Hash::make('12345678'), // Contraseña
+            'role' => 'director', // Rol Importante
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 2. Crear un Profesor de prueba
+        User::create([
+            'name' => 'Profesor Juan',
+            'email' => 'profe@escuela.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'profesor',
         ]);
     }
 }
